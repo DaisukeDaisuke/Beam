84fa1281e977e1665c94051c9606d60bcd774b15
DaisukeDaisuke <akblcmakblcm@gmail.com>
2018-03-01 21:42:12 +0900

    update  beam.php
    少しコミットを戻す&公式のpmmpで動くように

--- src/Managon/beam.php
+++ src/Managon/beam.php
@@ -17,6 +17,7 @@ use pocketmine\level\particle\RedstoneParticle;
 use pocketmine\level\particle\LargeExplodeParticle;
 use pocketmine\utils\Config;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\event\player\PlayerInteractEvent;
 
 class beam extends PluginBase implements Listener{
      private $speed = 0.8;
@@ -25,13 +26,9 @@ class beam extends PluginBase implements Listener{
      private $explode = false;
      private $attack = true;
      private $damage = 3;
-     private $weightsave = true;
-     private $i;
-     private $taskid;
 
      public function onEnable(){
      	Server::getInstance()->getPluginManager()->registerEvents($this, $this);
-     	$this->server = Server::getInstance();
       if(!file_exists($this->getDataFolder())){
       @mkdir($this->getDataFolder(), 0744, true);
   }
@@ -39,12 +36,11 @@ class beam extends PluginBase implements Listener{
           "bound" => false,
           "explode" => false,
           "attack" => true,
-          "damage" => 3,
-          "weight_save" => true));
-        $this->_toVar();
+          "damage" => 3));
+        $this->_toArray();
      }
 
-     public function _toVar(){
+     public function _toArray(){
         $bound = $this->con->get("bound");
 
         if($bound) $this->bound = true;
@@ -60,16 +56,12 @@ class beam extends PluginBase implements Listener{
         else $this->attack = false;
         
         $this->damage = (Int) $this->con->get("damage")*2;
-        
-        $this->weightsave = (bool) $this->con->get("weight_save");
-        if($this->weightsave) $this->speed = 2;
      }
 
-     public function onReceive(DataPacketReceiveEvent $event){
-     	$packet = $event->getPacket();
-     	$player = $event->getPlayer();
-     	if($packet instanceof UseItemPacket){
-         if($player->getInventory()->getItemInHand()->getId() === 280){
+     public function onReceive(PlayerInteractEvent $event){
+         $player = $event->getPlayer();
+         $name = $player->getName();
+         if($player->getInventory()->getItemInHand()->getId() == 280 && $player->getInventory()->getItemInHand()->getDamage() == 0){
            $x = $player->x;
            $y = $player->y + 1.35;
            $z = $player->z;
@@ -79,79 +71,44 @@ class beam extends PluginBase implements Listener{
            $this->py = $y;
            $this->pz = $z;
            $this->moveHook($yaw,$pitch,$player->getLevel(), $player);
-     		}
-  	}
-     }
+  }
+}
 
      public function moveHook($yaw, $pitch, Level $level, Player $player){
-      if(-$pitch > 40) $pitch += -6;//微調整
-      elseif(-$pitch <= -40) $pitch += 10;//微調整
-      if($this->weightsave)
-      {
-      	$this->taskid = $this->server->getScheduler()->scheduleRepeatingTask(new ws($this, $yaw, -$pitch, $level), 1)->getTaskId();
-      	return;
-      }
-      
+      if(-$pitch > 40) $plus = -6;//微調整
+      elseif(-$pitch <= -40) $plus = 10;//微調整
+      else $plus = 0;
       for($c = 0; $c <= 1400; $c++){
-       $pos = $this->getNextPos($yaw, -$pitch);
+       $y = tan(deg2rad(-$pitch+$plus))*$this->speed;
+       $base_t = ($this->speed**2 - $y ** 2) ** 0.5;
+       $x = cos(deg2rad($yaw+90))*$base_t;
+       $z = sin(deg2rad($yaw+90))*$base_t;
+       $pos = new Vector3($x + $this->px, $y + $this->py, $z + $this->pz);
        $particle = new RedstoneParticle($pos, 4);
        $level->addParticle($particle);
-       $this->px += $pos->x;
-       $this->py += $pos->y;
-       $this->pz += $pos->z;
        if($level->getBlock($pos)->getId() !== 0){
-       		if($this->bound) $this->beam_returnPitch($yaw, $pitch, $level, $pos);
-       	$e = new ex($this, $pos, $level);
-       	$this->server->getScheduler()->scheduleDelayedTask($e, 8);
-       	break;
-       }elseif($this->attack){
-      		foreach($level->getPlayers() as $p){
-        		if($p !== $player){
-        			if($pos->distance($p) < 2){
-        			$ev = new EntityDamageByEntityEvent($player, $p, 1, $this->damage, 0.4);
-          			$p->attack($this->damage, $ev);
-          			break;
-		 		}
-      			}
-      		}
-    		}
-     	}
+       if($this->bound) $this->beam_returnPitch($c, $yaw, $pitch, $level, $pos);
+       $e = new ex($this, $pos, $level);
+       Server::getInstance()->getScheduler()->scheduleDelayedTask($e, 8);
+       break;
+     }elseif($this->attack){
+      foreach($level->getPlayers() as $p){
+        if($p !== $player){
+        if($pos->distance($p) < 2){
+          $ev = new EntityDamageByEntityEvent($player,$p, 1, $this->damage, 0.4);
+          $p->attack($ev);
+          break;
+        }
+      }
+      }
+    }
+       $this->px += $x;
+       $this->py += $y;
+       $this->pz += $z;
+     }
      $this->px = 0;
      $this->py = 0;
      $this->pz = 0;
-    }
-    
-    public function moveHook_save($yaw, $pitch, Level $level)
-    {
-    	$this->i++;
-    	if($this->i == 50)
-    	{
-    		$this->i = 0;
-    		$this->server->getScheduler()->cancelTask($this->taskid);
-    		return;
-    	}
-    	$pos = $this->getNextPos($yaw, $pitch);
-    	$particle = new RedstoneParticle($pos, 4);
-        $level->addParticle($particle);
-        $this->px += $pos->x;
-        $this->py += $pos->y;
-        $this->pz += $pos->z;
-        if($level->getBlock($pos)->getId() !== 0){
-       		if($this->bound) $this->beam_returnPitch($yaw, $pitch, $level, $pos);
-       	$e = new ex($this, $pos, $level);
-       	$this->server->getScheduler()->scheduleDelayedTask($e, 8);
-       	break;
-       }elseif($this->attack){
-      		foreach($level->getPlayers() as $p){
-        		if($p !== $player){
-        			if($pos->distance($p) < 2){
-        			$ev = new EntityDamageByEntityEvent($player, $p, 1, $this->damage, 0.4);
-          			$p->attack($this->damage, $ev);
-          			break;
-		 		}
-      			}
-      		}
-    	}
     }
 
     public function e(Vector3 $pos, Level $level){
@@ -166,7 +123,7 @@ class beam extends PluginBase implements Listener{
           if($pos->distance($p) < rand(4, 5)){
             if($pos->distance($p) === 1){
               $ev = new EntityDamageByEntityEvent($player,$p, 1, $this->damage, 0.4);
-              $p->attack($this->damage, $ev);
+              $p->attack($ev);
             }
                 $ev = new EntityDamageEvent($p, 10, $e = (6 - $pos->distance($p)));
                 $p->attack($e, $ev);
@@ -175,22 +132,24 @@ class beam extends PluginBase implements Listener{
         }
       }
      }
+     $this->px = 0;
+     $this->py = 0;
+     $this->pz = 0;
     }
 
-    public function beam_returnPitch($yaw, $pitch, Level $level, Vector3 $pos1){
+    public function beam_returnPitch($count, $yaw, $pitch, Level $level, Vector3 $pos1){
       $this->px = $pos1->x;
       $this->py = $pos1->y;
       $this->pz = $pos1->z;
-      if($this->weightsave)
-      {
-      	$this->taskid = $this->server->getScheduler()->scheduleRepeatingTask(new ws($this, $yaw, $pitch), 1)->getTaskId();
-      	return;
-      }
       for($count = 0; $count <= 1400; $count++){
-       $pos = $this->getNextPos($yaw, $pitch);
-       $this->px += $pos->x;
-       $this->py += $pos->y;
-       $this->pz += $pos->z;
+       $y = tan(deg2rad($pitch))*$this->speed;
+       $base_t = ($this->speed**2 - $y ** 2) ** 0.5;
+       $x = cos(deg2rad($yaw+90))*$base_t;
+       $z = sin(deg2rad($yaw+90))*$base_t;
+       $this->px += $x;
+       $this->py += $y;
+       $this->pz += $z;
+       $pos = new Vector3($this->px, $this->py, $this->pz);
        $particle = new RedstoneParticle($pos, 4);
        $level->addParticle($particle);
       }
@@ -198,15 +157,6 @@ class beam extends PluginBase implements Listener{
      $this->py = 0;
      $this->pz = 0;
     }
-  }
-  
-  public function getNextPos($yaw, $pitch)
-  {
-  	$y = tan(deg2rad($pitch))*$this->speed;
-        $base_t = ($this->speed**2 - $y ** 2) ** 0.5;
-        $x = cos(deg2rad($yaw+90))*$base_t;
-        $z = sin(deg2rad($yaw+90))*$base_t;
-        return new Vector3($this->px + $x, $this->py + $y, $this->pz + $z);
   }
 
   class ex extends PluginTask{
@@ -220,20 +170,4 @@ class beam extends PluginBase implements Listener{
     public function onRun($currentTick){
       $this->owner->e($this->pos, $this->level);
     }
-  }
-  
-  class ws extends PluginTask{
-  	public function __construct(PluginBase $owner, $yaw, $pitch, Level $level)
-  	{
-  		parent::__construct($owner);
-  		$this->owner = $owner;
-  		$this->yaw = $yaw;
-  		$this->pitch = $pitch;
-  		$this->level = $level;
-  	}
-  	
-  	public function onRun($currentTick)
-  	{
-  		$this->owner->moveHook_save($this->yaw, $this->pitch, $this->level);
-  	}
   }
